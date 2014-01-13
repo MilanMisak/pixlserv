350fb6090a8f60d492ccc2e4b0ea043e5831ee36
Milan Misak <misakmilan@gmail.com>
2014-01-13 11:57:37 +0000

    Serving local files

--- server.go
+++ server.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
-import "os"
-import "github.com/codegangsta/martini"
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"net/http"
+	"os"
+
+	"github.com/codegangsta/martini"
+)
 
 const (
 	LOCAL_IMAGES_PATH = "local-images"
@@ -10,15 +18,29 @@ const (
 
 func main() {
 	m := martini.Classic()
-	m.Get("/image/**", func(params martini.Params) string {
+	m.Get("/image/**", func(params martini.Params) (int, string) {
 		imagePath := params["_1"]
 
-		log("Looking for: " + LOCAL_IMAGES_PATH + "/" + imagePath)
-
 		if _, err := os.Stat(LOCAL_IMAGES_PATH + "/" + imagePath); os.IsNotExist(err) {
-			return "image does not exist: " + imagePath
+			return http.StatusNotFound, "Image not found: " + imagePath
 		} else {
-			return "image exists: " + imagePath
+			reader, err := os.Open(LOCAL_IMAGES_PATH + "/" + imagePath)
+			if err != nil {
+				return http.StatusInternalServerError, "Cannot open image"
+			}
+			image, format, err := image.Decode(reader)
+			if err != nil {
+				return http.StatusInternalServerError, "Cannot decode image"
+			}
+
+			var buffer bytes.Buffer
+			if format == "jpeg" {
+				jpeg.Encode(&buffer, image, nil)
+			} else {
+				png.Encode(&buffer, image)
+			}
+
+			return http.StatusOK, buffer.String()
 		}
 	})
 	m.Run()
