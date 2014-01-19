ad2730f30593b67c709d159804a8d0719599fa0e
Milan Misak <misakmilan@gmail.com>
2014-01-19 21:13:54 +0000

    Parsing parameters

--- server.go
+++ server.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"image"
 	"image/jpeg"
 	"image/png"
+	"log"
 	"net/http"
 	"os"
+	"strings"
+	"strconv"
 
 	"github.com/codegangsta/martini"
 )
@@ -17,8 +19,19 @@ const (
 )
 
 func main() {
+	// Set up logging
+	log.SetPrefix("[pixlserv] ")
+	log.SetFlags(0)  // Removed the timestamp
+	log.Println("Test")
+
+	// Run the server
 	m := martini.Classic()
-	m.Get("/image/**", func(params martini.Params) (int, string) {
+	m.Get("/image/:parameters/**", func(params martini.Params) (int, string) {
+		parameters, err := parseParameters(params["parameters"])
+		if err != "" {
+			return http.StatusBadRequest, err
+		}
+		log.Println(parameters)
 		imagePath := params["_1"]
 
 		if _, err := os.Stat(LOCAL_IMAGES_PATH + "/" + imagePath); os.IsNotExist(err) {
@@ -33,6 +46,8 @@ func main() {
 				return http.StatusInternalServerError, "Cannot decode image"
 			}
 
+			// TODO - magic
+
 			var buffer bytes.Buffer
 			if format == "jpeg" {
 				jpeg.Encode(&buffer, image, nil)
@@ -46,6 +61,31 @@ func main() {
 	m.Run()
 }
 
-func log(message string) {
-	fmt.Println("[pixlserv] " + message)
+// Turns a string like "w_400,h_300" into a map[w:400 h:300]
+// The second return value is an error message
+// Also validates the parameters to make sure they have valid values
+// w = width, h = height
+func parseParameters(parametersStr string) (map[string]string, string) {
+	parameters := make(map[string]string)
+	parts := strings.Split(parametersStr, ",")
+	for _, part := range parts {
+		// TODO - validation
+		keyAndValue := strings.SplitN(part, "_", 2)
+		key := keyAndValue[0]
+		value := keyAndValue[1]
+
+		switch key {
+		case "w", "h":
+			value, err := strconv.Atoi(value)
+			if err != nil {
+				return nil, "Could not parse value for parameter: " + key
+			}
+			if value <= 0 {
+				return nil, "Value must be > 0: " + key
+			}
+		}
+
+		parameters[key] = value
+	}
+	return parameters, ""
 }
