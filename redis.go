56a97dc5fe7acd8ff4838d05f9e85410b2df1505
Milan Misak <misakmilan@gmail.com>
2014-05-26 21:53:30 +0100

    Connecting to redis using a given URL

--- redis.go
+++ redis.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	redisPortEnvVar  = "PIXLSERV_REDIS_PORT"
+	redisUrlEnvVar   = "PIXLSERV_REDIS_URL"
 	redisDefaultPort = 6379
 )
 
@@ -17,12 +18,20 @@ var (
 )
 
 func redisInit() error {
-	port, err := strconv.Atoi(os.Getenv(redisPortEnvVar))
-	if err != nil {
-		port = redisDefaultPort
+	url := os.Getenv(redisUrlEnvVar)
+	var err error
+	if url != "" {
+		Conn, err = redis.Dial("tcp", url)
+		return err
+	} else {
+		port, err := strconv.Atoi(os.Getenv(redisPortEnvVar))
+		if err != nil {
+			port = redisDefaultPort
+		}
+
+		Conn, err = redis.Dial("tcp", ":"+strconv.Itoa(port))
+		return err
 	}
-	Conn, err = redis.Dial("tcp", ":"+strconv.Itoa(port))
-	return err
 }
 
 func redisCleanUp() {
